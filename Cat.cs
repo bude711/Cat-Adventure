fe288d38c8cae222eeb8687dca123b289c889cce
t-csherman <t-csherman@LAPTOP-4FK84IG6>
2024-01-30 13:26:07 -0800

    setting up required classes for mvp

---
+++ Cat.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Cat_Adventure
+{
+    internal class Cat
+    {
+        public Cat()
+        {
+            Console.WriteLine("Cat created!");
+        }
+    }
+}
---
+++ GameManager.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Cat_Adventure
+{
+    internal class GameManager
+    {
+        public GameManager() 
+        { }
+
+        public void Run()
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
