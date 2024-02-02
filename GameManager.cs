02ebdab2a786358f3778babeb7576796e847c378
Christine Joo <t-cjoo@microsoft.com>
2024-02-01 17:14:54 -0800

    fixed readKey bug, removed comments, added readKey in reachedDestination

--- GameManager.cs
+++ GameManager.cs
@@ -38,7 +38,7 @@ namespace Cat_Adventure
             outputProvider.WriteLine("=========================\n");
             GraphicalStuff.WelcomeScreen(outputProvider);
             outputProvider.WriteLine("Press a key to continue\n");
-            Console.ReadKey();
+            Console.ReadKey(true);
             
             GameState = GameState.GameStarted;
             cat = new Cat(inputProvider, outputProvider);
@@ -60,45 +60,6 @@ namespace Cat_Adventure
                 Console.ReadKey();
             }
         }
-
-        //public void chooseDestination() {
-        //    //outputProvider.WriteLine("You are a cat and you want to go meet up with your cat friends.");
-        //    outputProvider.WriteLine($"You are a {cat.name}, a {cat.state} {cat.color} cat and you want to go meet up with your cat friends.");
-        //    outputProvider.WriteLine("Where do you want to go look for them?");
-        //    outputProvider.WriteLine();
-        //    outputProvider.WriteLine("1. the Park");
-        //    outputProvider.WriteLine("2. the Bodega");
-        //    outputProvider.WriteLine("3. the Alleyway");
-        //    outputProvider.WriteLine();
-        //    outputProvider.WriteLine("Please enter the number you would like to choose.");
-
-        //    var choice = inputProvider.Read();
-        //    if (choice.Contains('1')) {
-        //        outputProvider.WriteLine("You’ve chosen to go to the Park!");
-        //        outputProvider.WriteLine();
-        //        outputProvider.WriteLine("You exit your home and start your journey!");
-        //    }
-        //    else if (choice.Contains('2')) {
-        //        outputProvider.WriteLine("You’ve chosen to go to the Bodega!");
-        //        outputProvider.WriteLine();
-        //        outputProvider.WriteLine("You exit your home and start your journey!");
-        //    }
-        //    else if (choice.Contains('3'))
-        //    {
-        //        outputProvider.WriteLine("You’ve chosen to go to the Alleyway!");
-        //        outputProvider.WriteLine();
-        //        outputProvider.WriteLine("You exit your home and start your journey!");
-        //    }
-        //    else
-        //    {
-        //        outputProvider.WriteLine("Not a valid option. The cat gods have made the decision for you and you will be going to the dump.");
-        //    }
-        //    outputProvider.WriteLine();
-        //    outputProvider.WriteLine("Hit any key to continue.");
-        //    Console.ReadKey();
-
-        //    GraphicalStuff.WalkingCat(outputProvider);
-        //}
 
         public void handleDog()
         {
@@ -147,9 +108,10 @@ namespace Cat_Adventure
             outputProvider.WriteLine("Congratulations! You've arrived at your destination.");
             outputProvider.WriteLine("Your friends are here!");
             outputProvider.WriteLine("Now it's time for a cat party!");
+            outputProvider.WriteLine();
+            outputProvider.WriteLine("Hit any key to continue.");
+            Console.ReadKey();
             GraphicalStuff.FenceCats(outputProvider);
-
-
         }
     }
 }
